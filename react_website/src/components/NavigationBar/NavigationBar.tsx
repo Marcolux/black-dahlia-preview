c5e14fc8229f4aa86a080cffabae6ff659e75e6e
Marcolux <mccolucci1@gmail.com>
2025-11-07 13:12:45 -0600

    Pictures and home page work fonts

--- react_website/src/components/NavigationBar/NavigationBar.tsx
+++ react_website/src/components/NavigationBar/NavigationBar.tsx
@@ -4,31 +4,40 @@ import './navigation-bar.scss'
 const NavigationBar = () => {
     const location = useLocation()
     const isHome = location.pathname === '/'
-
-    
-
     return (
         <>  
         {
             isHome 
             ?
             <div className="navBar homeBar">
-                <img id="homepage_logo" src={`${process.env.PUBLIC_URL}/images/black_dahlia_logo_v1.jpg`} alt="Black Dahlia Creative logo" />
-                <NavLink className="navLinks" to={'/'}><p>Home Page</p></NavLink>
-                <NavLink className="navLinks" to={'/about'}><p>About</p></NavLink>
-                <NavLink className="navLinks" to={'/services'}><p>Services</p></NavLink>
-                <NavLink className="navLinks" to={'/portfolio'}><p>Portfolio</p></NavLink>
-                <NavLink className="navLinks" to={'/contact'}><p>Contact</p></NavLink>
-                <NavLink className="navLinks" to={'/membership'}><p>Membership</p></NavLink>
+                <div id="upper_home_bar"
+                >
+                    <img id="homepage_logo" src={`${process.env.PUBLIC_URL}/images/black_dahlia_logo_v1.jpg`} alt="Black Dahlia Creative logo"/>
+                    <div id="dahlia_logo_wrapper">
+                        <img id="homepage_dahlia_logo" src={`${process.env.PUBLIC_URL}/images/dahlia_icon_logo.svg`} alt="Black Dahlia Creative logo"/>
+                    </div>
+                    <button className="primaryBtn"> GET IN TOUCH</button>
+                </div>
+                <div id="homePageLinks" className="flex">
+                    {/* <NavLink className="navLinks" to={'/'}><p>Home Page</p></NavLink> */}
+                    <NavLink className="navLinks" to={'/about'}><p>About</p></NavLink>
+                    <NavLink className="navLinks" to={'/services'}><p>Services</p></NavLink>
+                    <NavLink className="navLinks" to={'/portfolio'}><p>Portfolio</p></NavLink>
+                    <NavLink className="navLinks" to={'/contact'}><p>Contact</p></NavLink>
+                    <NavLink className="navLinks" to={'/membership'}><p>Membership</p></NavLink>
+                </div>
             </div>
             :
             <div className="navBar notHome">
-                <NavLink className="navLinks" to={'/'}><p>Home Page</p></NavLink>
-                <NavLink className="navLinks" to={'/about'}><p>About</p></NavLink>
-                <NavLink className="navLinks" to={'/services'}><p>Services</p></NavLink>
-                <NavLink className="navLinks" to={'/portfolio'}><p>Portfolio</p></NavLink>
-                <NavLink className="navLinks" to={'/contact'}><p>Contact</p></NavLink>
-                <NavLink className="navLinks" to={'/membership'}><p>Membership</p></NavLink>
+                <img id="navbar_logo" src={`${process.env.PUBLIC_URL}/images/white_logo 1.png`} alt="Black Dahlia Creative logo White"/>
+                 <div id="homePageLinks" className="flex">
+                    <NavLink className="navLinks" to={'/'}><p>Home Page</p></NavLink>
+                    <NavLink className="navLinks" to={'/about'}><p>About</p></NavLink>
+                    <NavLink className="navLinks" to={'/services'}><p>Services</p></NavLink>
+                    <NavLink className="navLinks" to={'/portfolio'}><p>Portfolio</p></NavLink>
+                    <NavLink className="navLinks" to={'/contact'}><p>Contact</p></NavLink>
+                    <NavLink className="navLinks" to={'/membership'}><p>Membership</p></NavLink>
+                 </div>
             </div>
         }
         </>
--- react_website/src/pages/HomePage/HomePage.tsx
+++ react_website/src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,37 @@
+import { Link } from "react-router-dom"
 import "../page.scss" 
 import "./homepage.scss"
-import TimeLineStory from "../../components/TimelineStory/TimelineStory"
 
 const HomePage = () => {
 
     return(
         <div className="page" id="HomePage">
-            <h1>HOME PAGE</h1>
-                BLACK DAHLIA PREVIEW SITE
-            <h4>Time Line Story</h4>
-            <TimeLineStory className="my-20"/>
+            <section id="home_hero">
+                <img src={`${process.env.PUBLIC_URL}/images/home-page/black-dahlia-landing_page_mockup.webp`}  alt="black dahlia landing hero" />
+                <button id="landingBtn" className="primaryBtn">BOOK NOW</button>
+            </section>
+            <section id="offer">
+                <h1>What We Offer</h1>
+                <div id="offer-cards-wrapper" className="flex flex-wrap">
+                    <div className="offer-cards"><p>LOGO DESIGN</p></div>
+                    <div className="offer-cards"><p>BRAND IDENTITY</p></div>
+                    <div className="offer-cards"><p>WEB DESIGN</p></div>
+                    <div className="offer-cards"><p>BOOK CREATION</p></div>
+                    <div className="offer-cards"><p>MENU DESIGN</p></div>
+                    <div className="offer-cards"><p>CUSTOM PLANNER</p></div>
+                    <div className="offer-cards"><p>EVENT BRANDING</p></div>
+                </div>
+                <button className="secondaryBtn">
+                    <Link to={'/services'}>Explore Services <img src={`${process.env.PUBLIC_URL}/images/icons/Arrow-icon.png`}/></Link>
+                </button>
+            </section>
+            <section id="transition-img">
+                <img src={`${process.env.PUBLIC_URL}/images/home-page/black-dahlia-landing_page_mockup_2.webp`} alt="Black Dahlia on street wall" />
+            </section>
+            <section id="why-black-dahlia">
+                <h1>why black dahlia</h1>
+                <div></div>
+            </section>
         </div>
     )
 }
