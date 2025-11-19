69cb7c930d2494bc6a679453f32b368befecb6ed
Marcolux <mccolucci1@gmail.com>
2025-11-19 13:46:53 -0600

    Fixing Coming soon view

--- react_website/src/App.tsx
+++ react_website/src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 
-import NavigationBar from './components/NavigationBar/NavigationBar';
-import NavigationBarSmallScreen from './components/NavigationBarSmallScreen/NavigationBarSmallScreen';
+// import NavigationBar from './components/NavigationBar/NavigationBar';
+// import NavigationBarSmallScreen from './components/NavigationBarSmallScreen/NavigationBarSmallScreen';
 import HomePage from './pages/HomePage';
-import Page2 from './pages/Page2';
-import Page1 from './pages/Page1';
+// import Page2 from './pages/Page2';
+// import Page1 from './pages/Page1';
 
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 
 import './style/App.scss';
 import { useEffect, useState } from 'react';
@@ -27,21 +27,21 @@ function App() {
 
   return (
     <div className="App flex flex-column col-12">
-      {
+      {/* {
         smallScreenView === 'Regular'
         ?
         <NavigationBar/>
         :
-        <NavigationBarSmallScreen/>
-      }
+        <></>
+      } */}
       
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/Carousels" element={<Page1 />} />
-        <Route path="/page_2" element={<Page2 />} />
+        {/* <Route path="/Carousels" element={<Page1 />} />
+        <Route path="/page_2" element={<Page2 />} /> */}
       </Routes>
 
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   )
 }
--- react_website/src/pages/HomePage.tsx
+++ react_website/src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
 import "./page.scss" 
-import TimeLineStory from "../components/TimelineStory/TimelineStory"
 
 const HomePage = () => {
 
     return(
-        <div className="page">
-            <h1>HOME PAGE</h1>
+        <div className="page flex flex-justifyContent-center flex-alignItems-center">
+            <img src={`${process.env.PUBLIC_URL}/images/Dahlia_icon 2.svg`} alt="logo"  style={{width: '300px', height: 'auto'}}  className="spin"/>
 
-            <h4>Time Line Story</h4>
-            <TimeLineStory className="my-20"/>
+            <h1 className="fontSize40 col-12 text-center" id="coming_soon">- BLACK DAHLIA CREATIVE -</h1>
+                
+            <h4 className="fontSize30 mt-10 col-12 text-center" id="under_construction"> Website under construction </h4>
         </div>
     )
 }
