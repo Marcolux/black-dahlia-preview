a15cfda118e27f0a00dad43f9b330cf6f4e86147
Marcolux <mccolucci1@gmail.com>
2025-12-23 19:20:09 -0600

    Added icons to small menu

--- react_website/src/components/NavigationBarSmallScreen/NavigationBarSmallScreen.tsx
+++ react_website/src/components/NavigationBarSmallScreen/NavigationBarSmallScreen.tsx
@@ -2,6 +2,7 @@ import { NavLink, useLocation } from "react-router-dom";
 import './navigation-bar-small-screen.scss'
 import { useState } from "react";
 import { useTransition, animated } from "@react-spring/web";
+import { ReactComponent as Dahlia } from "./mandala_dahlia.svg";
 
 const NavigationBarSmallScreen = () => {
     const [isExpanded, setIsExpanded] = useState(false)
@@ -31,26 +32,14 @@ const NavigationBarSmallScreen = () => {
                 condition ? (
                     <animated.div style={style} id="smallScreenMenu" >
                         <div className='navBarSm'>
-                            <img
-                                className="imgNavMenu mb-30"
-                                src={`${process.env.PUBLIC_URL}/images/home-page/dahlia_icon_lightgray.webp`}
-                                alt={`Dahlia Nav Menu`}
-                                loading="lazy" 
-                                decoding="async"
-                            />
+                            <Dahlia  className="aboutDahlia mb-50" />
                             <NavLink className="navLinksSmScreen" to={'/'} onClick={toggleLogic}><p>Home Page</p></NavLink>
                             <NavLink className="navLinksSmScreen" to={'/about'} onClick={toggleLogic}><p>About</p></NavLink>
                             <NavLink className="navLinksSmScreen" to={'/services'} onClick={toggleLogic}><p>Services</p></NavLink>
                             <NavLink className="navLinksSmScreen" to={'/portfolio'} onClick={toggleLogic}><p>Portfolio</p></NavLink>
                             <NavLink className="navLinksSmScreen" to={'/contact'} onClick={toggleLogic}><p>Contact</p></NavLink>
                             <NavLink className="navLinksSmScreen" to={'/membership'} onClick={toggleLogic}><p>Membership</p></NavLink>
-                            <img
-                                className="imgNavMenu mt-30"
-                                src={`${process.env.PUBLIC_URL}/images/home-page/dahlia_icon_lightgray.webp`}
-                                alt={`Dahlia Nav Menu`}
-                                loading="lazy" 
-                                decoding="async"
-                            />
+                            <Dahlia  className="aboutDahlia mt-50" />
                         </div>
                     </animated.div>
                 ) : null
